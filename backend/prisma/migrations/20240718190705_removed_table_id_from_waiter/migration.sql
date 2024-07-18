2c1dcd942ce054072d6b32d319da4774af3bf5a7
unknown <olucas.cdev@gmail.com>
2024-07-18 16:08:17 -0300

    removed_table_id_from_waiter

---
+++ backend/prisma/migrations/20240718190705_removed_table_id_from_waiter/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `tableId` on the `waiter` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `waiter` DROP COLUMN `tableId`;
