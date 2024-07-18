4841b95335eb9279fe9ddd6169c7102f988aace7
unknown <olucas.cdev@gmail.com>
2024-07-18 15:30:19 -0300

    optmization

--- backend/src/services/storeServices/createStoreService.ts
+++ backend/src/services/storeServices/createStoreService.ts
@@ -9,25 +9,16 @@ interface CreateStoreDTO {
 class CreateStoreService {
   async execute({ storeName, storeImage, storeTableAmount, userId }: CreateStoreDTO) {
 
-    const status: string = "offline";
-    let imageUrl: string = "";
-
     if (!storeName || !storeTableAmount || !userId ) {
       throw new Error("Error: Please fill in all fields");
-    }
-
-    if (storeImage === "") {
-      imageUrl = "no image";
-    } else {
-      imageUrl = storeImage
     }
 
     const store = await prismaClient.store.create({
       data: {
         storeName,
-        storeImage: imageUrl,
+        storeImage: storeImage || "no image available",
         storeTableAmount,
-        storeStatus: status,
+        storeStatus: "offline",
         userId
       },
     });
