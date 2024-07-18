4cd124048f976142775e228209f9db2b2a18bfaf
unknown <olucas.cdev@gmail.com>
2024-07-18 17:34:14 -0300

    adjust n optimization login services

--- backend/src/services/loginServices/LoginService.ts
+++ backend/src/services/loginServices/LoginService.ts
@@ -15,47 +15,48 @@ interface AuthenticateUserServiceParams {
 }
 
 class AuthenticateUserService {
-  async execute({ loginEmail, loginPassword,accountType }: AuthenticateUserServiceParams) {
 
+  private async findUserByEmail(accountType: "admin" | "employee", email: string) {
+    if (accountType === "admin") {
+
+      return prismaClient.user.findUnique({ where: { userEmail: email } });
+
+    } else if (accountType === "employee") {
+
+      return prismaClient.waiter.findUnique({ where: { waiterEmail: email } });
+    }
+    throw new Error("Invalid account type");
+  }
+
+  async execute({ loginEmail, loginPassword, accountType }: AuthenticateUserServiceParams) {
     if (!loginEmail || !loginPassword || !accountType) {
       throw new Error("Please fill in all fields");
     }
 
-    let user;
-    let userPassword;
-    let userType;
+    const user = await this.findUserByEmail(accountType, loginEmail);
 
-    switch (accountType) {
-      case "admin":
-        user = await prismaClient.user.findUnique({
-          where: { userEmail: loginEmail },
-        });
-        userPassword = user?.userPassword;
-        userType = "admin";
-        break;
-
-      case "employee":
-        user = await prismaClient.waiter.findUnique({
-          where: { waiterEmail: loginEmail },
-        });
-        userPassword = user?.waiterPassword;
-        userType = "employee";
-        break;
-
-      default:
-        throw new Error("Invalid account type");
-    }
-
-    if (!user || !userPassword) {
+    if (!user) {
       throw new Error("Invalid email or password");
     }
 
-    const passwordMatch = await bcrypt.compare(loginPassword, userPassword);
-    if (!passwordMatch) {
+    let userPassword: string | undefined;
+    if (accountType === "admin") {
+      userPassword = (user as any).userPassword;
+
+    } else if (accountType === "employee") {
+      userPassword = (user as any).waiterPassword; 
+
+    } else {
+      throw new Error("Invalid account type");
+    }
+
+    if (!userPassword || !(await bcrypt.compare(loginPassword, userPassword))) {
       throw new Error("Invalid password");
     }
 
-    const token = jwt.sign({ userId: user.id, userType }, SECRET_KEY, {
+    const userId = accountType === "admin" ? user.id : (user as any).storeId;
+
+    const token = jwt.sign({ userId, userType: accountType }, SECRET_KEY, {
       expiresIn: "5m",
     });
 
