6443c77cbb037a057188fefb425bbc9deda5b3d1
unknown <olucas.cdev@gmail.com>
2024-07-19 17:40:33 -0300

    change float to decimal and add migration

---
+++ backend/prisma/migrations/20240719203923_change_float_to_decimal/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `costumertab` MODIFY `tabValue` DECIMAL(10, 2) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `item` MODIFY `itemValue` DECIMAL(10, 2) NOT NULL;
+
+-- AlterTable
+ALTER TABLE `order` MODIFY `orderValue` DECIMAL(10, 2) NOT NULL;
