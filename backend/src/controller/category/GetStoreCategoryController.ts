d108fd58371003784b8d4a77db3e702a4b4e1c06
unknown <olucas.cdev@gmail.com>
2024-07-19 20:14:06 -0300

    adjust storeId search method

--- backend/src/controller/category/GetStoreCategoryController.ts
+++ backend/src/controller/category/GetStoreCategoryController.ts
@@ -1,12 +1,15 @@
 import { Request, Response } from "express";
 import { GetStoreCategoryService } from "../../services/categoryServices/GetStoreCategoryService";
+import { verifyToken } from "../../autentication/Auth";
+import { JwtPayload } from "jsonwebtoken";
 
 class GetStoreCategoryController {
   async handle(req: Request, res: Response) {
-    const { storeId } = req.body;
+    const token = req.headers["authorization"]?.replace("Bearer ", "");
     const categoryService = new GetStoreCategoryService();
 
-    const result = await categoryService.execute({ storeId });
+    const decodedToken: JwtPayload | null = verifyToken(token as any);
+    const result = await categoryService.execute({ storeId: decodedToken?.storeId });
     return res.status(201).json(result);
   }
 };
