80e75d6551f90fe835032f3d4015b723f31d66fa
unknown <olucas.cdev@gmail.com>
2024-08-23 12:39:57 -0300

    adjust global styles

--- frontend/src/GlobalStyles.ts
+++ frontend/src/GlobalStyles.ts
@@ -5,10 +5,10 @@ const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    color: #fff;
     font-family: "Inter", sans-serif;
 
   }
+  p, h1, h3, h2, div, input{ color: #fff}
 
   body {
     background-color: #141414;
