2ef3d4c88a0360bf1a3a4db4b88c01b35b9751ef
unknown <olucas.cdev@gmail.com>
2024-08-25 19:44:18 -0300

    stylize dashboard page

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -1,7 +1,146 @@
+import React from "react";
+import { Content, Menu } from "./styles";
+import { FaCircle } from "react-icons/fa";
+import { IoIosArrowForward } from "react-icons/io";
+import { MdListAlt, MdOutlineFastfood, MdOutlineSpaceDashboard, MdOutlineTableBar } from "react-icons/md";
+
 function Dashboard() {
-  return ( 
-    <h1>Dashboard</h1>
-   );
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  return (
+    <>
+      <Menu menu={isMenuOpen}>
+        <div className="header">
+          <img src="https://t4.ftcdn.net/jpg/00/65/77/27/360_F_65772719_A1UV5kLi5nCEWI0BNLLiFaBPEkUbv5Fv.jpg" />
+
+          <div className="profile">
+            <h1>Lorem Ipsum</h1>
+            <p>On <FaCircle color="#00e700" /></p>
+          </div>
+
+        </div>
+        <div className="navigator">
+          <a href="#">
+            <span>
+              <MdOutlineSpaceDashboard size={26} />
+              Dashboard
+            </span>
+            <IoIosArrowForward />
+          </a>
+          <a href="#">
+            <span>
+              <MdListAlt size={26} />
+              Pedidos
+            </span>
+            <IoIosArrowForward />
+          </a>
+          <a href="#">
+            <span>
+              <MdOutlineFastfood size={26} />
+              Cardápio
+            </span>
+            <IoIosArrowForward />
+          </a>
+          <a href="#">
+            <span>
+              <MdOutlineTableBar size={26} />
+              Mesas
+            </span>
+            <IoIosArrowForward />
+          </a>
+        </div>
+        <div className="activeMenu" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+          <IoIosArrowForward size={40} />
+        </div>
+      </Menu>
+      <Content>
+        <div className="introduce">
+          <h2>Olá "Nome de Usuário</h2>
+          <p>Acompanhe suas estatísticas de hoje.</p>
+        </div>
+
+        <div className="container">
+          <div className="performance">
+            <h2>Meu desempenho</h2>
+
+            <div className="statistics">
+              <div className="bloco1">
+                <div className="header">
+                  <p>Número de pedidos:</p>
+                </div>
+                <div className="content">
+                  <p>10</p>
+                </div>
+              </div>
+              <div className="bloco2">
+                <div className="header">
+                  <p>Faturamento:</p>
+                </div>
+                <div className="content">
+                  <p>10</p>
+                </div>
+              </div>
+              <div className="bloco3">
+                <div className="header">
+                  <p>Total de clientes:</p>
+                </div>
+                <div className="content">
+                  <p>10</p>
+                </div>
+              </div>
+              <div className="bloco4">
+                <div className="header">
+                  <p>Mesas disponíveis:</p>
+                </div>
+                <div className="content">
+                  <p>10</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="orders">
+            <h2>Meus pedidos</h2>
+
+            <div className="statistics">
+              <div className="bloco1">
+                <div className="header">
+                  <p>Em análise:</p>
+                </div>
+                <div className="content">
+                  <p>10</p>
+                </div>
+              </div>
+              <div className="bloco2">
+                <div className="header">
+                  <p>Em produção:</p>
+                </div>
+                <div className="content">
+                  <p>10</p>
+                </div>
+              </div>
+              <div className="bloco3">
+                <div className="header">
+                  <p>Finalizados:</p>
+                </div>
+                <div className="content">
+                  <p>10</p>
+                </div>
+              </div>
+              <div className="bloco4">
+                <div className="header">
+                  <p>Comandas em aberto:</p>
+                </div>
+                <div className="content">
+                  <p>10</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Content>
+    </>
+  );
 }
 
 export default Dashboard;
---
+++ frontend/src/pages/Dashboard/styles.ts
@@ -0,0 +1,252 @@
+import styled from "styled-components";
+
+interface IMenu {
+  menu: boolean;
+}
+
+export const Menu = styled.div<IMenu>`
+  width: ${(props) => (props.menu ? "300px" : "70px")};
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 2em;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-color: #191919;
+  box-shadow: 2px 0 3px #00000059;
+  transition: 0.3s ease-in-out;
+  overflow-y: auto;
+  overflow-x: hidden;
+
+  &::-webkit-scrollbar {
+    width: 12px;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: #191919;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background-color: #353535;
+    border-radius: 20px;
+    border: 3px solid #191919;
+  }
+
+  div.header {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1em;
+
+    img {
+      width: 70%;
+      height: auto;
+      padding: ${(props) => (props.menu ? "2em" : ".5em")};
+      transition: 1s;
+      border-radius: 50%;
+    }
+
+    div.profile {
+      width: 100%;
+      padding: 0 1em;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      justify-content: center;
+
+      h1 {
+        display: ${(props) => (props.menu ? "block" : "none")};
+      }
+
+      p {
+        display: flex;
+        align-items: center;
+        gap: 0.3em;
+      }
+    }
+  }
+
+  div.navigator {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    a {
+      width: 100%;
+      padding: 1em 1.4em;
+      color: #fff;
+      text-decoration: none;
+      display: flex;
+      justify-content: space-between;
+      border-bottom: ${(props) => (props.menu ? "1px solid gray" : "none")};
+      transition: all 0.1s ease-in;
+
+      &:hover {
+        color: #d6313b;
+        background-color: #212121;
+      }
+
+      span {
+        display: flex;
+        align-items: center;
+        gap: 1.5em;
+      }
+    }
+  }
+
+  div.activeMenu {
+    cursor: pointer;
+    width: fit-content;
+    height: fit-content;
+    position: fixed;
+    top: 50%;
+    left: ${(props) => (props.menu ? "300px" : "70px")};
+    transform: ${(props) => (props.menu ? "0" : "rotateY(190deg)")};
+    transition: 0.3s ease-in-out;
+  }
+
+  @media (max-width: 760px) {
+    width: 100%;
+    height: auto;
+    gap: 0;
+    transform: ${(props) => (props.menu ? "0" : "translateY(-400px)")};
+    border-bottom-left-radius: 2em;
+    border-bottom-right-radius: 2em;
+    box-shadow: 0 2px 3px #00000059;
+    overflow: visible;
+
+    div.header {
+      flex-direction: row;
+      padding: 1em;
+      background-color: #101010;
+      img {
+        width: 25vw;
+        padding: 0;
+      }
+      div.profile {
+        h1 {
+          font-size: 1.2em;
+        }
+        p {
+          font-size: 0.8em;
+        }
+      }
+    }
+    div.activeMenu {
+      width: 100%;
+      height: fit-content;
+      position: ${(props) => (props.menu ? "relative" : "fixed")};
+      top: ${(props) => (props.menu ? "none" : "400px")};
+      left: 0;
+      transform: none;
+      text-align: center;
+      background-color: #212121;
+      transition: 1s all ease-in-out;
+      z-index: 1000;
+
+      svg {
+        transition: 1.5s ease-in-out;
+        transform: ${(props) =>
+          props.menu ? "rotate(-90deg)" : "rotate(90deg)"};
+      }
+    }
+  }
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding-left: 350px;
+  background-color: #121212;
+
+  div.introduce {
+    padding: 5em 0;
+    display: flex;
+    flex-direction: column;
+    gap: 0.7em;
+
+    p {
+      color: gray;
+    }
+  }
+  .container {
+    display: flex;
+    flex-direction: column;
+    gap: 2em;
+  }
+  div.performance,
+  div.orders {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 1em;
+
+    div.statistics {
+      display: flex;
+      flex-wrap: wrap;
+      align-items: center;
+      gap: 1em;
+    }
+    div.bloco1,
+    div.bloco2,
+    div.bloco3,
+    div.bloco4 {
+      width: 250px;
+      border-radius: 0.5em;
+      overflow: hidden;
+      .header {
+        width: 100%;
+        padding: 0.5em 0.5em;
+        background-color: #4aa7ff;
+        p {
+          color: #000;
+          font-weight: bold;
+        }
+      }
+      .content {
+        padding: 0.5em 0;
+        background-color: #212121;
+        text-align: center;
+        p {
+          font-size: 2em;
+          font-weight: bold;
+        }
+      }
+    }
+  }
+
+  @media (max-width: 1200px) {
+    padding-left: 15vw;
+  }
+  @media (max-width: 760px) {
+    padding: 0 5vw;
+
+    div.statistics {
+      justify-content: center;
+    }
+    div.performance,
+    div.orders {
+      div.bloco1,
+      div.bloco2,
+      div.bloco3,
+      div.bloco4 {
+        width: 150px;
+
+        .header {
+          padding: 0.4em;
+          p {
+            font-size: 0.7em;
+          }
+        }
+        .content {
+          p {
+            font-size: 1.5em;
+          }
+        }
+      }
+    }
+  }
+`;
